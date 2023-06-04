9b86948fa73709b91cc7635d0435fd0bd822730d
Liubov <pandora1705@mail.ru>
2023-06-04 16:31:40 +0400

    HW4

---
+++ HW4.java
@@ -0,0 +1,17 @@
+public class HW4 {
+
+  public static void main(String[] args) {
+    Tree<Integer> tree = new Tree<>();
+    tree.add(11);
+    tree.add(25);
+    tree.add(35);
+    tree.add(75);
+    tree.add(10);
+    tree.add(1);
+    tree.add(2);
+    tree.add(3);
+    tree.add(4);
+    tree.add(5);
+    tree.print();
+  }
+}
